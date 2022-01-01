76c9358ede684c4765dc666a62c68aa37e33f45b
William Marks <68719434+WMcoding-MW@users.noreply.github.com>
2021-12-31 17:33:35 -0700

    finished the home section
    added a home section with a description of what my portfolio consists of, mainly style.css changes.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(() => {
         $('#menu').click(function () {
-            $(this).toggleClass('fa-angle-down'); //changes the symbol of menu when clicked
-            $('header').toggleClass('toggle');
+            $(this).toggleClass('fa-angle-left'); //changes the symbol of menu when clicked
+            $('header').toggleClass('toggle'); 
         });
+
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa-angle-left'); //removes the menu when scrolled a certain distance
+        $('header').removeClass('toggle'); 
+    });
 
     });
